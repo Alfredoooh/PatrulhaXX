7b1803124f2649a4a6ba9b3f4bae19a387d964ff
Alfredoooh <alfredopjonas@gmail.com>
2026-03-06 22:46:25 +0100

    Atualizar o settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,7 +19,8 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    // Kotlin atualizado para 2.1.0 — evita o warning do Flutter
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
